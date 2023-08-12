5b1a923d6dc2c3caf281e8401d2d683f7f2f9f99
Isaac Dzordi <dzordinetwork@gmail.com>
2023-08-12 17:43:53 -0300

    Updates Final Beta Free

--- imagesroll.js
+++ imagesroll.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   setupImageHover('block1', ['./assets/freemode1.jpg', './assets/freemode1night.jpg']);
   setupImageHover('block2', ['./assets/freemode2.jpg', './assets/freemode2fps.jpg', './assets/freemode2night.jpg']);
   setupImageHover('block3', ['./assets/freemode3.jpg', './assets/freemode3night.jpg', './assets/freemode3fps.jpg']);
-  setupImageHover('block4', ['./assets/lottus1.jpg', './assets/lottus2.jpg']);
+  setupImageHover('block4', ['./assets/freemode4.jpg', './assets/freemode4night.jpg', './assets/freemode4fps.jpg', './assets/freemode4night2.jpg']);
   setupImageHover('block5', ['./assets/prime1.jpg', './assets/prime2.jpg']);
   setupImageHover('finalblock', ['./assets/vippanel.jpg', './assets/vippanel2.jpg', './assets/vippanel3.jpg', './assets/vippanel4.jpg', './assets/vippanel5.jpg' ]);
 });
